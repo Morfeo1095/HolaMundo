0fba5d0c891205e79f252aced5a64677ae287cb8
Juan <juan95carlos@gmail.com>
2023-04-17 19:03:21 +0200

    Se crea la clase Persona main

---
+++ src/com/acme/Persona.java
@@ -0,0 +1,23 @@
+package com.acme;
+
+public class Persona {
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s", nombre);
+    }
+}
